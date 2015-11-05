e9eb79b724d691443f32ae6797b9ee0db2e850cd
ChrisBurgin <chrisburgin95@gmail.com>
2015-11-05 14:03:14 -0500

    Minimum Window Size

--- main.js
+++ main.js
@@ -21,7 +21,10 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 1400, height: 700});
+  mainWindow.setMinimumSize(1000, 600);
+
+
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/templates/index.html');
